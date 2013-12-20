c849eded99bde9d86453b3e11491c02a42c66d2e
Gal Madar <madargal@gmail.com>
2013-12-20 04:13:10 +0200

    added Product class to FIrstServlet

--- TryOnJenkinsPrject/src/main/java/servlets/FirstServlet.java
+++ TryOnJenkinsPrject/src/main/java/servlets/FirstServlet.java
@@ -8,12 +8,16 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import entities.Product;
+
 @WebServlet(urlPatterns="/FirstServlet/*")
 public class FirstServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-
+	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.getOutputStream().write("Hello From Servlet!!!".getBytes());
+		Product p = new Product();
+		p.setName("gal madar");
+		response.getOutputStream().write(p.getName().getBytes());
 	}
 
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
